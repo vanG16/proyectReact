e731fdc8ba678920d9e3112758482334059f630f
VanG16 <xvgc2000cbta3@gmail.com>
2021-03-02 16:16:13 -0600

    commitdos

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Google Maps App</h1>
+      <h1>Garcia Cisneros Xitlaly Vanessa</h1>
       <GoogleMap />
       <Helmet>
       <script defer data-key="6aead1cb-199e-4174-88d5-9fef74d734c0" src="https://widget.tochat.be/bundle.js"></script>        
