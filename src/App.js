de7289119ab2658e2b2b86d7aa7048e6a0bc16d9
VanG16 <xvgc2000cbta3@gmail.com>
2021-03-02 16:12:04 -0600

    commituno

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import GoogleMap from './components/GoogleMap';
+import {Helmet} from "react-helmet";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Maps App</h1>
+      <GoogleMap />
+      <Helmet>
+      <script defer data-key="6aead1cb-199e-4174-88d5-9fef74d734c0" src="https://widget.tochat.be/bundle.js"></script>        
+       </Helmet>
+    </div>
+  );
+}
+
+export default App;
